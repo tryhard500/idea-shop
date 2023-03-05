f85865e8df61ec6fe3efd6a01db0372d507304db
Nikita Baskov <nb500205@gmail.com>
2023-03-05 12:16:02 +0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,119 @@
+products = [
+{
+    title: 'Кактус',
+    price: '200',
+    img: 'assets/goods/1.jpg'
+},
+{
+    title: 'Настенные часы',
+    price: '1500',
+    img: 'assets/goods/2.jpg'
+},
+{
+    title: 'Телевизор',
+    price: '25000',
+    img: 'assets/goods/3.jpg'
+},
+{
+    title: 'Блендер',
+    price: '1200',
+    img: 'assets/goods/4.jpg'
+},
+{
+    title: 'Шторы',
+    price: '3000',
+    img: 'assets/goods/5.jpg'
+},
+{
+    title: 'Комплект посуды',
+    price: '5000',
+    img: 'assets/goods/6.jpg'
+},
+
+];
+//Вызовы функций
+toggleCardButton();
+renderCards();
+search();
+addToCart();
+
+
+function addToCart(){
+    let cards = document.querySelectorAll(`.card`);
+    let sidebarListItems = [];
+    for (let i = 0; i < cards.length; i++){
+        cards[i].addEventListener('click',()=>{
+        if (!cards[i].classList.contains('card-active')) {
+            cards[i].classList.add('card-active');
+            if (!sidebarListItems.includes(i)) {
+                sidebarListItems.push(i);
+            }
+        } else {
+            cards[i].classList.remove('card-active');
+            if (sidebarListItems.includes(i)) {
+                sidebarListItems.pop(i);
+            }
+        }
+        renderItemsInCard(sidebarListItems);
+        });
+    }
+}
+
+function renderItemsInCard(sidebarListItems){
+    let sidebarList = document.querySelector(`.sidebar-list`);
+    let sidebarTotal = document.querySelector(`.sidebar-total`);
+    let price = 0;
+    for (let i = 0; i < sidebarListItems; i++){
+        id = sidebarListItems[i];
+        sidebarList.innerHTML += `
+            <li>${products[id].title} - ${products[id].price}<li>
+        `;
+        price += Number(products[sidebarListItems[i]].price);
+    }
+    if (price != 0) {
+        sidebarTotal.innerHTML = price;
+    }
+}
+
+//Функции
+function toggleCardButton(){
+    document
+    .querySelector(`.cart-button`)
+    .addEventListener('click',function(){
+        document.querySelector(`.sidebar`).classList.toggle('hidden')
+    });
+}
+
+function renderItem(i){
+    document.querySelector(`.food-container`).innerHTML += `
+        <div class="card">
+        <img src="${products[i].img}">
+        <div class="card-body">
+            <span class="food-title">${products[i].title}</span>
+            <span class="food-price">${products[i].price}</span>
+        </div>
+        </div>
+    `;
+}
+
+function renderCards(){
+    for (let i = 0; i < products.length; i++){
+        renderItem(i);
+    }
+}
+
+function searchByTitle(){
+    let search = document.querySelector(`.search-input`).value.toLowerCase();
+    document.querySelector(`.food-container`).innerHTML = ``;
+    for (let i = 0; i < products.length; i++){
+        if (products[i].title.toLowerCase().includes(search)) {
+            renderItem(i);
+        } 
+}
+}
+
+function search(){
+    document
+    .querySelector(`.search-input`)
+    .addEventListener(`input`,searchByTitle);
+}
